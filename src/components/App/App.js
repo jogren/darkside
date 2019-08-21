5fa088314b80ff105573ad20a73da902131e49b7
Djavan Munroe <djavanm24@gmail.com>
2019-08-21 17:00:37 -0600

    Fix missing commits and update fetch calls

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,42 +2,47 @@ import React, { Component } from 'react';
 import Nav from '../Nav/Nav';
 import Container from '../Container/Container';
 import './App.css';
+import apiCalls from '../Util/apiCalls'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      planets: []
+      planets: [],
+      people: [],
+      vehicles: [],
+      favorites: [],
+      crawl: ''
     }
   }
 
   componentDidMount() {
+    let randomNumber = Math.floor((Math.random() * 7) + 1)
+    fetch(`https://swapi.co/api/films/${randomNumber}`)
+      .then(res => res.json())
+      .then(data => this.setState({crawl: data.opening_crawl}))
+
     fetch('https://swapi.co/api/planets/')
       .then(res => res.json())
-      .then(data => this.cleanData(data.results))
-      .then(cleanedData => this.setState({ planets: cleanedData }))
-    }
-    
-    cleanData(data) {
-      return data.map(planet => {
-        let namesArray = [];
-        planet.residents.map(person => {
-          return fetch(person)
-          .then(res => res.json())
-          .then(data => namesArray.push(data.name))
-        });
-        return {
-          name: planet.name,
-          terrain: planet.terrain,
-          population: planet.population,
-          climate: planet.climate,
-          residents: namesArray
-        }
-      });
+      .then(data => apiCalls.cleanPlanetData(data.results))
+      .then(planets => this.setState({ planets }))
+
+    fetch('https://swapi.co/api/people/')
+      .then(res => res.json())
+      .then(data => apiCalls.cleanPeopleData(data.results))
+      .then(data => apiCalls.getPeopleHomes(data))
+      .then(data => apiCalls.getSpecies(data))
+      .then(people => this.setState({ people }))
+
+    fetch('https://swapi.co/api/vehicles/')
+      .then(res => res.json())
+      .then(data => apiCalls.cleanVehicles(data.results))
+      .then(vehicles => this.setState({ vehicles }))
+  
     }
     
     render() {
-      console.log(this.state.planets)
+      console.log(this.state)
       return (
         <main className="App-main">
         <header className="App-header">
