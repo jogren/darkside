b4f552986b2724cbd0356173ed528724e8d9ff7e
Jacob Ogren <jogren0044@gmail.com>
2019-08-22 13:39:10 -0600

    Add hover states, background image and updated svgs

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,15 +7,15 @@ const Nav = () => {
     <nav>
       <div className="Nav_div Nav-people">
         <p className="Nav_p">People</p>
-        <img src="https://i.pinimg.com/originals/6e/df/0f/6edf0f66c1248e21cb7695cccaafbe35.png" />
+        <img src="https://icon-library.net/images/starwars-icon/starwars-icon-4.jpg" />
       </div>
       <div className="Nav_div Nav-planets">
         <p className="Nav_p">Planets</p>
-        <img src="https://i.ebayimg.com/images/g/GS0AAOSwqVVbMde7/s-l300.png" />
+        <img src="http://icons.iconarchive.com/icons/sensibleworld/starwars/1024/Death-Star-icon.png" />
       </div>
       <div className="Nav_div Nav-vehicles">
         <p className="Nav_p">Vehicles</p>
-        <img src="https://dtkp6g0samjql.cloudfront.net/uploads/photo/file/11974902/gallery_hero_52e00645-0d84-4bd5-afd0-341aad91e53e.jpg" />
+        <img src="https://cdn2.iconfinder.com/data/icons/star-wars-6/24/Tie-Fighter-512.png" />
       </div>
       <div className="Nav_div Nav-favorites">
         <p className="Nav_p">Favorites</p>
